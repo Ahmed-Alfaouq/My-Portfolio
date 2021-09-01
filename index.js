cf4aeca3c4c0a6ae7350bfaacfa0e38258434fbd
AhmedOmarAL-farouq <ahmed.omar.alfarouq@gmail.com>
2021-09-01 11:40:37 +0200

    Fix css linter errors

--- index.js
+++ index.js
@@ -67,10 +67,9 @@ const modal = {
     'Ruby on rails',
   ],
   text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer 
-  took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. 
-  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-  it 1960s with the releax map lapora verita.`,
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+  when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent`,
   image: {
     src: './images/modal.png',
     alt: 'modal image',
@@ -80,20 +79,28 @@ const modal = {
 const modalContainer = document.createElement('div');
 modalContainer.setAttribute('class', 'modal d-none');
 // Title
-const modalTitle = document.createElement('h2');
-modalTitle.setAttribute('class', 'modal-title crete-font');
-modalTitle.textContent = modal.title;
-modalContainer.appendChild(modalTitle);
+const modalTitleBs = document.createElement('h2');
+modalTitleBs.setAttribute('class', 'modal-title-bs crete-font');
+modalTitleBs.textContent = modal.title;
+modalContainer.appendChild(modalTitleBs);
+
+const modalTitleSs = document.createElement('h2');
+modalTitleSs.setAttribute('class', 'modal-title-ss crete-font');
+modalTitleSs.textContent = 'Multi Post Stories';
+modalContainer.appendChild(modalTitleSs);
 // Close Icon
 const modalCloseIcon = document.createElement('i');
 modalCloseIcon.className = 'fas fa-times';
 modalContainer.appendChild(modalCloseIcon);
 // List
 const modalList = document.createElement('ul');
-modalList.setAttribute('class', 'list inter-font');
+modalList.setAttribute('class', 'modal-list inter-font');
 const listItem1 = document.createElement('li');
 const listItem2 = document.createElement('li');
 const listItem3 = document.createElement('li');
+listItem1.className = 'modal-list-item';
+listItem2.className = 'modal-list-item';
+listItem2.className = 'modal-list-item';
 listItem1.textContent = modal.list[0];
 listItem2.textContent = modal.list[1];
 listItem3.textContent = modal.list[2];
@@ -113,9 +120,9 @@ modalContainer.appendChild(imageContainer);
 
 // Content
 const contentContainer = document.createElement('div');
-contentContainer.setAttribute('class', 'content');
+contentContainer.setAttribute('class', 'modal-content');
 const contentText = document.createElement('p');
-contentText.setAttribute('class', 'inter-font');
+contentText.setAttribute('class', 'inter-font description');
 contentText.textContent = modal.text;
 contentContainer.appendChild(contentText);
 const liveBtn = document.createElement('a');
